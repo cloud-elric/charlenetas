7cf51081d32a59a3c7068e4f0a6efa484ea45382
Raul Alanis <rulosc07@gmail.com>
2016-09-23 15:18:13 -0500

    cambios de id en etiqueas

--- modules/modAdminPanel/views/admin/soloPorHoy.php
+++ modules/modAdminPanel/views/admin/soloPorHoy.php
@@ -3,19 +3,32 @@ use app\models\EntPosts;
 use app\models\EntComentariosPosts;
 use app\modules\modAdminPanel\assets\ModuleAsset;
 use yii\web\View;
+
+$this->title = '<i class="ion ion-android-calendar"></i> Solo por hoy';
 ?>
 
 <!-- .page-cont -->
 <div class="page-cont">
 
+
 	<div class="row" id="js-contenedor-tarjetas">
 	<?php foreach ($postsSoloPorHoy as $postSoloPorHoy){?>
 		<div class="col s12 m6 l4" id="card_<?=$postSoloPorHoy->txt_token?>">
 			<div class="card card-solo-por-hoy" data-token="<?=$postSoloPorHoy->txt_token?>">
-				<h3><?=$postSoloPorHoy->txt_descripcion?></h3>
-				<p><?=EntComentariosPosts::find ()->where ( [ 'id_post' => $postSoloPorHoy->id_tipo_post ] )->andWhere ( [ 'is','id_comentario_padre',null ] )->count ( "id_post" )?> comentarios</p>
-				<div class="card-options">
-					<div class="card-options-check">
+				
+
+				<div class="card-contexto-cont">
+					<p class="card-desc"><?=$postSoloPorHoy->txt_descripcion?></p>
+				</div>
+
+				<div class="card-contexto-status">
+					<p class="card-contexto-status-comen">
+						<i class="ion icon icon-comment"></i> <span><?=EntComentariosPosts::find ()->where ( [ 'id_post' => $postSoloPorHoy->id_tipo_post ] )->andWhere ( [ 'is','id_comentario_padre',null ] )->count ( "id_post" )?></span>
+					</p>
+				</div>
+
+				<div class="card-contexto-options">
+					<div class="card-contexto-options-check">
 						<input type="checkbox" class="filled-in" id="filled-in-box3"
 							checked="checked" /> <label for="filled-in-box3"></label>
 					</div>
@@ -23,20 +36,21 @@ use yii\web\View;
 						<i class="ion ion-android-more-vertical card-edit"></i>
 					</a>
 				</div>
+
 			</div>
 		</div>
-<?php }?>
+		<?php }?>
 
-						<!-- <div class="col s12">
-							<a class="modal-trigger waves-effect waves-light btn" href="#modal1">Modal</a>
-						</div> -->
+		<!-- <div class="col s12">
+			<a class="modal-trigger waves-effect waves-light btn" href="#modal1">Modal</a>
+		</div> -->
 
 	</div>
 
 
 
 	<div class="fixed-action-btn horizontal">
-		<a class="btn-floating btn-large waves-effect waves-light btn-check modal-trigger" href="#js-modal-post">
+		<a class="btn-floating btn-large waves-effect waves-light btn-agregar modal-trigger" href="#js-modal-post">
 			<i class="ion ion-wand"></i>
 		</a>
 	</div>
--- modules/modAdminPanel/views/admin/verdadazos.php
+++ modules/modAdminPanel/views/admin/verdadazos.php
@@ -3,6 +3,8 @@ use app\models\EntPosts;
 use app\models\EntComentariosPosts;
 use app\modules\modAdminPanel\assets\ModuleAsset;
 use yii\web\View;
+
+$this->title = '<i class="ion ion-ios-paper"></i> Verdadazos';
 ?>
 
 <!-- .page-cont -->
@@ -11,12 +13,21 @@ use yii\web\View;
 	<div class="row" id="js-contenedor-tarjetas">
 <?php foreach ( $postsVerdadazos as $postVerdadazos ) {?>
 		<div class="col s12 m6 l4" id="card_<?=$postVerdadazos->txt_token?>">
+
 			<div class="card card-verdadazos" data-token="<?=$postVerdadazos->txt_token?>">
-				<h3><?=$postVerdadazos->txt_descripcion?></h3>
-				<p><?=EntComentariosPosts::find ()->where ( [ 'id_post' => $postVerdadazos->id_tipo_post ] )->andWhere ( [ 'is','id_comentario_padre',null ] )->count ( "id_post" )?> comentarios</p>
 				
-				<div class="card-options">
-					<div class="card-options-check">
+				<div class="card-contexto-cont">
+					<p class="card-desc"><?=$postVerdadazos->txt_descripcion?></p>
+				</div>
+
+				<div class="card-contexto-status">
+					<p class="card-contexto-status-comen">
+						<i class="ion icon icon-comment"></i> <span><?=EntComentariosPosts::find ()->where ( [ 'id_post' => $postVerdadazos->id_tipo_post ] )->andWhere ( [ 'is','id_comentario_padre',null ] )->count ( "id_post" )?></span>
+					</p>
+				</div>
+
+				<div class="card-contexto-options">
+					<div class="card-contexto-options-check">
 						<input type="checkbox" class="filled-in" id="filled-in-box1"
 							checked="checked" /> <label for="filled-in-box1"></label>
 					</div>
@@ -27,11 +38,11 @@ use yii\web\View;
 			</div>
 		</div>
 
-<?php }?>
+	<?php }?>
 
 	</div>
 	<div class="fixed-action-btn horizontal">
-		<a class="btn-floating btn-large waves-effect waves-light btn-check modal-trigger" href="#js-modal-post">
+		<a class="btn-floating btn-large waves-effect waves-light btn-agregar modal-trigger" href="#js-modal-post">
 			<i class="ion ion-wand"></i>
 		</a>
 	</div>
@@ -41,22 +52,22 @@ use yii\web\View;
 <!-- end /.page-cont -->
 
 <?php
-foreach ( $postsVerdadazos as $postVerdadazos ) {
-	echo $postVerdadazos->txt_descripcion . "   ";
-	echo $postVerdadazos->txt_imagen . "   ";
-	echo $postVerdadazos->num_likes . "   ";
-	echo $postVerdadazos->fch_creacion . "   ";
-	echo $postVerdadazos->fch_publicacion . "   ";
+// foreach ( $postsVerdadazos as $postVerdadazos ) {
+// 	echo $postVerdadazos->txt_descripcion . "   ";
+// 	echo $postVerdadazos->txt_imagen . "   ";
+// 	echo $postVerdadazos->num_likes . "   ";
+// 	echo $postVerdadazos->fch_creacion . "   ";
+// 	echo $postVerdadazos->fch_publicacion . "   ";
 	
-	echo "</br>";
-	echo "</br>";
-}
-echo "total= " . EntPosts::find ()->where ( [ 
-		'id_tipo_post' => $postVerdadazos->id_tipo_post 
-] )->count ( "id_tipo_post" . "   " );
-echo "total likes= " . EntPosts::find ()->where ( [ 
-		'id_tipo_post' => $postVerdadazos->id_tipo_post 
-] )->sum ( "num_likes" );
+// 	echo "</br>";
+// 	echo "</br>";
+// }
+// echo "total= " . EntPosts::find ()->where ( [ 
+// 		'id_tipo_post' => $postVerdadazos->id_tipo_post 
+// ] )->count ( "id_tipo_post" . "   " );
+// echo "total likes= " . EntPosts::find ()->where ( [ 
+// 		'id_tipo_post' => $postVerdadazos->id_tipo_post 
+// ] )->sum ( "num_likes" );
 
 $bundle = ModuleAsset::register ( Yii::$app->view );
 $bundle->js [] = 'js/charlenetas-verdadazos.js'; // dynamic file added
