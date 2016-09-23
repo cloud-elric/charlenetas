8b890804933005778243b56953adc00a3bf859d7
cloud-elric <humberto@2gom.com.mx>
2016-09-23 10:50:19 -0500

    Se agrego materialize al proyecto (Antes apuntaba a internet)

--- modules/modAdminPanel/assets/ModuleAsset.php
+++ modules/modAdminPanel/assets/ModuleAsset.php
@@ -7,15 +7,17 @@ use yii\web\AssetBundle;
 class ModuleAsset extends AssetBundle {
 	public $sourcePath = '@app/modules/modAdminPanel/web/';
 	public $css = [ 
-			'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css',
+			//'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css',
 			'https://fonts.googleapis.com/icon?family=Material+Icons',
+			'materialize/css/materialize.min.css',
 			'css/ionicons.min.css',
 			'css/asScrollable.min.css',
 			'css/charlenetas.css',
 			'css/temporal.css'
 	];
 	public $js = [ 
-			'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js',
+			//'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js',
+			'materialize/js/materialize.min.js',
 			'js/jquery.mousewheel.min.js',
 			'js/jquery-asScrollable.min.js',
 			'js/charlenetas.js'
