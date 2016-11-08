5217756a568b058582ed619205a47d3ca5f97f47
damsSanch <damian2gom@gmail.com>
2016-11-07 18:30:05 -0600

    Crear usuarios - Lista de usuarios

--- modules/modAdminPanel/views/calendario/_crearCitas.php
+++ modules/modAdminPanel/views/calendario/_crearCitas.php
@@ -4,26 +4,6 @@ $this->title = 'Agenda';
 $this->icon = '<i class="ion ion-film-marker"></i>';
 ?>
 
-<!-- 
-<head>	
-	
-	<style>
-
-		body {
-			margin: 40px 10px;
-			padding: 0;
-			font-family: "Lucida Grande",Helvetica,Arial,Verdana,sans-serif;
-			font-size: 14px;
-		}
-
-		#calendar {
-			max-width: 900px;
-			margin: 0 auto;
-		}
-
-	</style>
-</head> -->
-
 <!-- .page-cont -->
 <div class="page-cont">
 
