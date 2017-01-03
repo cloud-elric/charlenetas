0a2660fbd5187af47385de69b518d746c7043b1a
damsSanch <damian2gom@gmail.com>
2017-01-03 15:13:10 -0600

    Checkbox en tutoriales

--- views/netas/index.php
+++ views/netas/index.php
@@ -407,10 +407,26 @@ if (! empty ($isCuentaActivada)) {
 	</div>
 	<!-- end - .modal-content -->
 
-	<!-- Brn Close -->
+	<!-- Btn Close -->
 	<span id="modal-tutoriales-close" class="modal-close">
 		<i class="icon-close"></i>
 	</span>
+
+	<ul class="list modal-check">
+		<li class="list__item">
+			<label class="label--checkbox">
+					
+				<div class="label--checkbox-row">
+					<div class="label--checkbox-row-col">
+						<input type="checkbox" id="checkbox" class="checkbox" checked>
+					</div>
+					<div class="label--checkbox-row-col">
+						No volver a mostrar
+					</div>
+				</div>
+			</label>
+		</li>
+	</ul>
 
 	<!-- <i class="material-icons medium icon-demo">trending_flat</i> -->
 
