da10c98688471301b3d0247b07dd34b70ba05b42
damsSanch <damian2gom@gmail.com>
2017-01-09 14:35:37 -0600

    Anuncios - Tutoriales - Grid

--- views/netas/include/_anuncioPin.php
+++ views/netas/include/_anuncioPin.php
@@ -2,17 +2,8 @@
 use yii\helpers\Url;
 ?>
 
-<div class="pin pin-sabias-que">
-	<div class="pin-header pin-header-sabias-que"></div>
-	
-	<div class="pin-content-wrapper" lang="en" style="background-image: url(<?=Url::base()?>/uploads/imagenesAnuncios/<?=$listaAnuncios[$countAnuncio]->txt_imagen?>)">
-		<p class="pin-descripcion">
-			ESTO ES UN ANUNCIO <?=$listaAnuncios[$countAnuncio]->id_anuncio?>
-		</p>
-		<div class="pin-link">
-			<a class="waves-effect waves-light btn btn-secondary"
-			href="#"
-			target="_blank">Ver nota</a>
-		</div>
-	</div>
+<!-- <div class="pin pin-anuncio pin-anuncio-250-250"> -->
+<div class="pin pin-anuncio pin-anuncio-250-400">
+	<div class="pin-header pin-header-anuncio"></div>
+	<div class="pin-content-wrapper" lang="en" style="background-image: url(<?=Url::base()?>/uploads/imagenesAnuncios/<?=$listaAnuncios[$countAnuncio]->txt_imagen?>)"></div>
 </div>
