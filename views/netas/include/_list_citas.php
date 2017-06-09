76013273eaecacc4515338a46d85748ececffb52
cloud-elric <humberto@2gom.com.mx>
2017-06-09 14:04:59 -0500

    Correciones de disenio

--- views/netas/include/_list_citas.php
+++ views/netas/include/_list_citas.php
@@ -17,6 +17,6 @@ foreach($meses as $key=>$value){
 	}
 }
 ?>
-<div class="col m5" style="margin:10px">
+<div class="col m6" style="margin:10px">
     <span><?= "FECHA: " . $fecha . " HORARIO: " . $horaInicio . " a " . $horaFin?></span>
 </div>
