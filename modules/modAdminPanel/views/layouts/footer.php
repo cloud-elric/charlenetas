5d122b5abaf1b04147474ac1af4e4cab11bfc01b
cloud-elric <humberto@2gom.com.mx>
2017-06-12 16:48:22 -0500

    Correciones menores de anio y un poco de hoja de estilo

--- modules/modAdminPanel/views/layouts/footer.php
+++ modules/modAdminPanel/views/layouts/footer.php
@@ -1,7 +1,7 @@
 <!-- .footer -->
 <div class="footer">
 	<div class="footer-cont">		
-		<p>&copy;  Charlenetas 2015. All rights reserved.</p>
+		<p>&copy;  Charlenetas <?=date("Y")?>>. All rights reserved.</p>
 		<p><a href="http://2gom.com.mx/" target="_blank">2 Geeks One Monkey</a></p>
 	</div>	
 </div>
